3f73f4963b6924047e22598fd1816a9e49861c60
hibahfatima <h3fatima@uwaterloo.ca>
2024-01-24 03:46:10 -0500

    Added more items and filtering functionality for catalog component

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -7,23 +7,63 @@ import { IProduct } from './product.model';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
-  product: IProduct;
+  products: IProduct[];
+  filter: string = '';
 
   constructor() {
-    this.product = {
-      id: 2,
-      description:
-        "Made of plastic. A rarity nowadays.",
-      effects: "3 ATK",
-      name: "Toy Knife",
-      imageName: "toy-knife.webp",
-      category: "Weapons",
-      price: 0,
-      discount: 0,
-    };
+    this.products = [
+      {
+        id: 2,
+        description:
+          "Made of plastic. A rarity nowadays.",
+        effects: "3 ATK",
+        name: "Toy Knife",
+        imageName: "toy-knife.webp",
+        category: "Weapons",
+        price: 0,
+        discount: 0,
+      },
+      {
+        id: 3,
+        description:
+          "A worn pink leather glove. For five-fingered folk.",
+        effects: "5 ATK",
+        name: "Tough Glove",
+        imageName: "tough-glove.webp",
+        category: "Weapons",
+        price: 50,
+        discount: 0,
+      },
+      {
+        id: 4,
+        description:
+          "Has a distinct, non-licorice flavor.",
+        effects: "10 HP",
+        name: "Monster Candy",
+        imageName: "monster-candy.png",
+        category: "Food",
+        price: 0,
+        discount: 0,
+      },
+      {
+        id: 5,
+        description:
+          "It has already been used several times. ",
+        effects: "10 HP",
+        name: "Bandage",
+        imageName: "bandage.png",
+        category: "Armour",
+        price: 0,
+        discount: 0,
+      }
+  ];
   }
 
   getImageUrl(product: IProduct) {
-    return '/assets/images/' + product.imageName;
+    return '/assets/images/undertale-items/' + product.imageName;
+  }
+
+  getFilteredProducts() {
+    return this.filter === '' ? this.products : this.products.filter((product) => product.category === this.filter);
   }
 }
